262d571f13ead4c43b0e27a7682555600e0b08cd
JoshuaTaye <tayejoshua4@gmail.com>
2024-10-08 11:03:54 +0300

    changed IP address of host. This will work for all android simulators.

--- role-based-backend/dist/auth/jwt.strategy.d.ts
+++ role-based-backend/dist/auth/jwt.strategy.d.ts
@@ -29,9 +29,9 @@ declare const JwtStrategy_base: new (...args: any[]) => any;
 export declare class JwtStrategy extends JwtStrategy_base {
     private userModel;
     constructor(userModel: Model<User>);
-    validate(payload: any): Promise<import("mongoose").Document<unknown, {}, User> & User & {
-        _id: import("mongoose").Types.ObjectId;
-    } & {
+    validate(payload: any): Promise<import("mongoose").Document<unknown, {}, User> & User & Required<{
+        _id: unknown;
+    }> & {
         __v?: number;
     }>;
 }
